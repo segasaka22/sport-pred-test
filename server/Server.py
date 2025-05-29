0d4bae96b84ffc183f49ef7ed68ae6439b3ca67c
segasaka22 <segarasaka@gmail.com>
2025-05-29 07:53:42 +0700

    Add files via upload

---
+++ server/Server.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from utils import pred, load_artifacts
+
+
+app = Flask(__name__)
+
+@app.route("/home")
+def hello():
+    return "<h1> Hello </h1>"
+
+@app.route("/prediction", methods = ['GET', 'POST'])
+def prediction():
+    image_data = request.form['image_data']
+
+    response = jsonify(pred(image_data))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+if __name__ == "__main__":
+    print("starting server")
+    load_artifacts()
+    app.run(port=5000)
